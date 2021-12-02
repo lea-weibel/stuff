45f2e467d8c31d3dae7e4db317f31b643632b65c
lea-weibel <78731556+lea-weibel@users.noreply.github.com>
2021-12-02 11:21:57 +0100

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+export default function IndexPage() {
+  return (
+    <div>
+      <span role="img" aria-label="construction emoji">
+        🚧
+      </span>{" "}
+      Under construction...
+    </div>
+  );
+}
